1f04165a2c80e9810015a772236d822a8d229040
izukuuuu <jianshuangxie@gmail.com>
2024-03-16 00:55:52 +0800

    modified:   index.html 	modified:   main.js 	modified:   style.css 	modified:   style_preset.css

--- main.js
+++ main.js
@@ -36,13 +36,156 @@ if (currentLanguage === 'en') {
 const elements = document.querySelectorAll('.text-gradient-1, .text-gradient-2, .text-gradient-3');
 
 const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('animate');
-        }
-    });
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate');
+    }
+  });
 });
 
 elements.forEach(element => {
-    observer.observe(element);
+  observer.observe(element);
+});
+
+// 标题栏显示切换
+window.addEventListener('scroll', function () {
+  const header = document.getElementById('chapter-1-header');
+  const targetElement = document.getElementById('chapter-1-title');
+  const bottomElement = document.getElementById('chapter-1-bottom');
+
+  // 获取目标元素的位置信息
+  const targetElementRect = targetElement.getBoundingClientRect();
+  const bottomElementRect = bottomElement.getBoundingClientRect();
+
+  // 当滚动到目标元素的顶部时显示标题栏
+  if (targetElementRect.bottom <= 0) {
+    header.classList.remove('hidden');
+    header.classList.add('visible');
+  } else {
+    header.classList.remove('visible');
+    header.classList.add('hidden');
+  }
+
+  // 当滚动到目标元素的底部时隐藏标题栏
+  if (bottomElementRect.bottom <= 0) {
+    header.classList.add('hidden');
+    header.classList.remove('visible');
+  }
+});
+
+window.addEventListener('scroll', function () {
+  const header = document.getElementById('chapter-2-header');
+  const targetElement = document.getElementById('chapter-2-title');
+  const bottomElement = document.getElementById('chapter-2-bottom');
+
+  // 获取目标元素的位置信息
+  const targetElementRect = targetElement.getBoundingClientRect();
+  const bottomElementRect = bottomElement.getBoundingClientRect();
+
+  // 当滚动到目标元素的顶部时显示标题栏
+  if (targetElementRect.bottom <= 0) {
+    header.classList.remove('hidden');
+    header.classList.add('visible');
+  } else {
+    header.classList.remove('visible');
+    header.classList.add('hidden');
+  }
+
+  // 当滚动到目标元素的底部时隐藏标题栏
+  if (bottomElementRect.bottom <= 0) {
+    header.classList.add('hidden');
+    header.classList.remove('visible');
+  }
+});
+
+window.addEventListener('scroll', function () {
+  const header = document.getElementById('chapter-3-header');
+  const targetElement = document.getElementById('chapter-3-title');
+  const bottomElement = document.getElementById('chapter-3-bottom');
+
+  // 获取目标元素的位置信息
+  const targetElementRect = targetElement.getBoundingClientRect();
+  const bottomElementRect = bottomElement.getBoundingClientRect();
+
+  // 当滚动到目标元素的顶部时显示标题栏
+  if (targetElementRect.bottom <= 0) {
+    header.classList.remove('hidden');
+    header.classList.add('visible');
+  } else {
+    header.classList.remove('visible');
+    header.classList.add('hidden');
+  }
+
+  // 当滚动到目标元素的底部时隐藏标题栏
+  if (bottomElementRect.bottom <= 0) {
+    header.classList.add('hidden');
+    header.classList.remove('visible');
+  }
+});
+
+window.addEventListener('scroll', function () {
+  const header = document.getElementById('chapter-4-header');
+  const targetElement = document.getElementById('chapter-4-title');
+  const bottomElement = document.getElementById('chapter-4-bottom');
+
+  // 获取目标元素的位置信息
+  const targetElementRect = targetElement.getBoundingClientRect();
+  const bottomElementRect = bottomElement.getBoundingClientRect();
+
+  // 当滚动到目标元素的顶部时显示标题栏
+  if (targetElementRect.bottom <= 0) {
+    header.classList.remove('hidden');
+    header.classList.add('visible');
+  } else {
+    header.classList.remove('visible');
+    header.classList.add('hidden');
+  }
+
+  // 当滚动到目标元素的底部时隐藏标题栏
+  if (bottomElementRect.bottom <= 0) {
+    header.classList.add('hidden');
+    header.classList.remove('visible');
+  }
+});
+
+window.addEventListener('scroll', function () {
+  const header = document.getElementById('icon-bar');
+  const targetElement = document.getElementById('chapter-1-title');
+
+  // 获取目标元素的位置信息
+  const targetElementRect = targetElement.getBoundingClientRect();
+
+  // 当滚动到目标元素的顶部时显示标题栏
+  if (targetElementRect.top <= 0) {
+    header.classList.remove('hidden');
+    header.classList.add('visible');
+  } else {
+    header.classList.remove('visible');
+    header.classList.add('hidden');
+  }
+});
+
+// 平滑滚动
+function scrollToElement(elementId) {
+  const element = document.getElementById(elementId);
+  element.scrollIntoView({ behavior: 'smooth' });
+}
+
+// 标题文本层级切换
+window.addEventListener('scroll', function() {
+  const titleText = document.querySelector('.title-text');
+  const content = document.getElementById('chapter-1');
+  
+  const titleTextRect = titleText.getBoundingClientRect();
+  const contentRect = content.getBoundingClientRect();
+  
+  // 如果滚动到下一个大的 div，则将 title-text 的 z-index 设置为低于 0，使其被下面的元素覆盖
+  if (contentRect.top <= titleTextRect.top) {
+      titleText.classList.add('hidden');
+      titleText.classList.remove('visible');
+  } else {
+      // 否则将 z-index 设置为较高的值，使其浮在最上层
+      titleText.classList.add('visible');
+      titleText.classList.remove('hidden');
+  }
 });
