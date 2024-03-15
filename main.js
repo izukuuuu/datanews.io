c04a609147645bf0173e6202c137122ec612725d
izukuuuu <jianshuangxie@gmail.com>
2024-03-15 22:51:57 +0800

    modified:   .DS_Store 	modified:   area-stack-gradient.html 	modified:   assets/js/main.min.js 	new file:   avatar_test.html 	renamed:    "\345\256\230\346\226\271\345\252\222\344\275\223_\351\242\221\346\254\241\345\211\21510\346\235\241\345\275\242\345\233\276_\345\220\210\345\271\266\345\256\230\345\252\222.html" -> "data/\345\256\230\346\226\271\345\252\222\344\275\223_\351\242\221\346\254\241\345\211\21510\346\235\241\345\275\242\345\233\276_\345\220\210\345\271\266\345\256\230\345\252\222.html" 	renamed:    "\345\276\256\345\215\232\347\224\250\346\210\267_\346\203\205\346\204\237\345\200\276\345\220\221\347\216\257\345\275\242\345\233\276.html" -> "data/\345\276\256\345\215\232\347\224\250\346\210\267_\346\203\205\346\204\237\345\200\276\345\220\221\347\216\257\345\275\242\345\233\276.html" 	renamed:    "\347\237\245\344\271\216_\346\203\205\346\204\237\345\200\276\345\220\221\347\216\257\345\275\242\345\233\276.html" -> "data/\347\237\245\344\271\216_\346\203\205\346\204\237\345\200\276\345\220\221\347\216\257\345\275\242\345\233\276.html" 	new file:   img/avatar1.png 	new file:   img/avatar2.png 	new file:   img/avatar3.png 	new file:   img/avatar4.png 	new file:   img/avatar5.png 	new file:   img/avatar6.png 	new file:   img/portrait1.png 	new file:   img/portrait2.png 	new file:   img/portrait3.png 	modified:   index.html 	modified:   index_en.html 	modified:   index_es.html 	modified:   index_pt.html 	modified:   main.js 	modified:   style.css 	modified:   style_preset.css

--- main.js
+++ main.js
@@ -1,22 +1,48 @@
-// 多语言选择
-document.getElementById('language').addEventListener('change', function() {
-    var selectedLanguage = this.value;
-    // Redirect to the corresponding language version of the page
-    switch(selectedLanguage) {
-      case 'zh':
-        window.location.href = 'index.html';
-        break;
-      case 'en':
-        window.location.href = 'index_en.html';
-        break;
-      case 'es':
-        window.location.href = 'index_es.html';
-        break;
-      case 'pt':
-        window.location.href = 'index_pt.html';
-        break;
-      default:
-        // 默认语言
-        window.location.href = 'index.html';
+// Dropdown 切换
+document.addEventListener("click", function (e) {
+  const dropdowns = document.querySelectorAll('.dropdown');
+  dropdowns.forEach(dropdown => {
+    if (!dropdown.contains(e.target)) {
+      const dropdownContent = dropdown.querySelector('.dropdown-content');
+      dropdownContent.style.display = 'none';
     }
   });
+});
+
+document.querySelectorAll('.dropbtn').forEach(button => {
+  button.addEventListener('click', function (e) {
+    const dropdownContent = this.nextElementSibling;
+    dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
+  });
+});
+
+// 多语言选择
+// 获取当前页面的语言
+var currentLanguage = window.location.pathname.split('.')[0].split('_')[1]; // 假设文件名为 index_en.html，index_pt.html 等
+
+// 更新当前语言显示
+var currentLanguageElement = document.getElementById('current-language');
+if (currentLanguage === 'en') {
+  currentLanguageElement.textContent = 'English';
+} else if (currentLanguage === 'es') {
+  currentLanguageElement.textContent = 'Español';
+} else if (currentLanguage === 'pt') {
+  currentLanguageElement.textContent = 'Português';
+} else {
+  currentLanguageElement.textContent = '中文';
+}
+
+// 文字渐变背景动画
+const elements = document.querySelectorAll('.text-gradient-1, .text-gradient-2, .text-gradient-3');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate');
+        }
+    });
+});
+
+elements.forEach(element => {
+    observer.observe(element);
+});
