cb14a9ac3d84fe064ba396accc36c20e26ab7eef
NateAFish <jianshangxie@gmail.com>
2024-04-15 16:45:20 +0800

    new file:   data/area.js 	modified:   index.html 	modified:   style.css

---
+++ data/area.js
@@ -0,0 +1,221 @@
+var dom = document.getElementById('container-area');
+var myChart = echarts.init(dom, null, {
+  renderer: 'canvas',
+  useDirtyRect: false
+});
+var app = {};
+
+var option;
+
+option = {
+color: ['#80FFA5', '#00DDFF', '#37A2FF', '#FF0087', '#FFBF00', '#9CCC65'],
+tooltip: {
+trigger: 'axis',
+axisPointer: {
+  type: 'cross',
+  label: {
+    backgroundColor: '#6a7985'
+  }
+}
+},
+legend: {
+data: ['亚洲', '非洲', '欧洲', '北美洲', '南美洲', '大洋洲']
+},
+grid: {
+left: '3%',
+right: '4%',
+bottom: '3%',
+containLabel: true
+},
+xAxis: [
+{
+  type: 'category',
+  boundaryGap: false,
+  data: ['2016', '2017', '2018', '2019', '2020', '2021', '2022']
+}
+],
+yAxis: [
+{
+  type: 'value'
+}
+],
+series: [
+{
+  name: '亚洲',
+  type: 'line',
+  stack: 'Total',
+  smooth: true,
+  lineStyle: {
+    width: 0
+  },
+  showSymbol: false,
+  areaStyle: {
+    opacity: 0.8,
+    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+      {
+        offset: 0,
+        color: 'rgb(128, 255, 165)'
+      },
+      {
+        offset: 1,
+        color: 'rgb(1, 191, 236)'
+      }
+    ])
+  },
+  emphasis: {
+    focus: 'series'
+  },
+  data: [160000, 170593, 189191, 205970, 175484, 141071, 130000]
+},
+{
+  name: '非洲',
+  type: 'line',
+  stack: 'Total',
+  smooth: true,
+  lineStyle: {
+    width: 0
+  },
+  showSymbol: false,
+  areaStyle: {
+    opacity: 0.8,
+    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+      {
+        offset: 0,
+        color: 'rgb(0, 221, 255)'
+      },
+      {
+        offset: 1,
+        color: 'rgb(77, 119, 255)'
+      }
+    ])
+  },
+  emphasis: {
+    focus: 'series'
+  },
+  data: [50000, 49651, 57407, 63463, 58412, 47511, 43000]
+},
+{
+  name: '欧洲',
+  type: 'line',
+  stack: 'Total',
+  smooth: true,
+  lineStyle: {
+    width: 0
+  },
+  showSymbol: false,
+  areaStyle: {
+    opacity: 0.8,
+    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+      {
+        offset: 0,
+        color: 'rgb(55, 162, 255)'
+      },
+      {
+        offset: 1,
+        color: 'rgb(116, 21, 219)'
+      }
+    ])
+  },
+  emphasis: {
+    focus: 'series'
+  },
+  data: [32000, 35324, 39071, 42079, 25698, 13026, 13000]
+},
+{
+  name: '北美洲',
+  type: 'line',
+  stack: 'Total',
+  smooth: true,
+  lineStyle: {
+    width: 0
+  },
+  showSymbol: false,
+  areaStyle: {
+    opacity: 0.8,
+    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+      {
+        offset: 0,
+        color: 'rgb(255, 0, 135)'
+      },
+      {
+        offset: 1,
+        color: 'rgb(135, 0, 157)'
+      }
+    ])
+  },
+  emphasis: {
+    focus: 'series'
+  },
+  data: [12000, 12935, 14736, 14211, 8383, 5435, 4400]
+},
+{
+  name: '南美洲',
+  type: 'line',
+  stack: 'Total',
+  smooth: true,
+  lineStyle: {
+    width: 0
+  },
+  showSymbol: false,
+  label: {
+    show: true,
+    position: 'top'
+  },
+  areaStyle: {
+    opacity: 0.8,
+    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+      {
+        offset: 0,
+        color: 'rgb(255, 191, 0)'
+      },
+      {
+        offset: 1,
+        color: 'rgb(224, 62, 76)'
+      }
+    ])
+  },
+  emphasis: {
+    focus: 'series'
+  },
+  data: [3000, 3460, 3702, 3920, 3374, 2434, 2300]
+},
+{
+  name: '大洋洲',
+  type: 'line',
+  stack: 'Total',
+  smooth: true,
+  lineStyle: {
+    width: 0
+  },
+  showSymbol: false,
+  label: {
+    show: true,
+    position: 'top'
+  },
+  areaStyle: {
+    opacity: 0.8,
+    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+      {
+        offset: 0,
+        color: 'rgb(220, 237, 200)'
+      },
+      {
+        offset: 1,
+        color: 'rgb(156, 204, 101)'
+      }
+    ])
+  },
+  emphasis: {
+    focus: 'series'
+  },
+  data: [3000, 2846, 3394, 3429, 2441, 1430, 2300]
+}
+]
+};
+
+
+if (option && typeof option === 'object') {
+  myChart.setOption(option);
+}
+
+window.addEventListener('resize', myChart.resize);
